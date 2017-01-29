84f341302a01b3a3365efdf2839309cf5844975b
gabsand <gabrielle.sanderson@gmail.com>
2017-01-28 20:45:13 -0600

    fixed some tslint errors

--- client/polyfills.ts
+++ client/polyfills.ts
@@ -18,15 +18,16 @@ interface IPolyFillErrorConstructor extends ErrorConstructor {
     stackTraceLimit: any;
 }
 
-if(!ENV) {
-    var ENV = 'development';
+var ENV;
+
+if ( !ENV ) {
+    ENV = 'development';
 }
 
-if(ENV === 'production') {
+if ( ENV === 'production' ) {
     // Production
 } else {
     // Development
-    
     (<IPolyFillErrorConstructor>Error).stackTraceLimit = Infinity;
     // require('zone.js/dist/long-stack-trace-zone');
 }
